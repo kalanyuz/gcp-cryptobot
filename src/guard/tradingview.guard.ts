bf7e5bd4a9440c61f5c24d14f204e0fa64698c70
KalanyuZ <kalanyuz@gmail.com>
2021-02-16 23:55:28 +0900

    Add guard to only allow incoming requests from Trading View

---
+++ src/guard/tradingview.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TradingViewGuard implements CanActivate {
+  constructor() {}
+
+  checkAllowedAddress(ipAddress: string): boolean {
+    const allowedAddress = [
+      '52.89.214.238',
+      '34.212.75.30',
+      '54.218.53.128',
+      '52.32.178.7',
+    ];
+    if (allowedAddress.find((val) => val === ipAddress) === undefined) {
+      console.error(`Received a request from suspicious address: ${ipAddress}`);
+      return false;
+    }
+    return true;
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const ctx = context.switchToHttp().getRequest<Request>();
+    const forwardedIp = ctx.headers['x-forwarded-for'];
+    return this.checkAllowedAddress(forwardedIp);
+  }
+}
