4520103f2c5849167963c083610e3a93f3ad1ad9
KalanyuZ <kalanyuz@users.noreply.github.com>
2021-05-07 22:45:42 +0900

    Add buy and sell for BitFlyer (#29)

---
+++ src/exchange/entities/exchange.ts
@@ -0,0 +1,17 @@
+/**
+ * I'd recommend to manually split the ticker into separate symbols
+ * when creating alerts in TradingView
+ * For alert placeholder details, refer to: https://www.tradingview.com/chart/?solution=43000531021
+ */
+export interface BotRequest {
+  // Asset of interests (ie. ETH from ticker ETHBTC)
+  asset: string;
+  // What your asset is priced in (ie. BTC from ticker ETHBTC)
+  denominator: string;
+  // Amount to sell (optional)
+  amount: number | undefined;
+  // Closing price (aka. {{close}} in TradingView)
+  price: number | undefined;
+  // Time of alert (aka. {{time}} in TradingView)
+  time: string | undefined;
+}
