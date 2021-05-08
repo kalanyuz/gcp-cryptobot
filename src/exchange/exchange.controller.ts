1125b1c61a3b395cf9f96f921ce1ce831030bed7
KalanyuZ <kalanyuz@users.noreply.github.com>
2021-05-08 22:42:14 +0900

    Feature/tradingview guard (#34)
    * Add guard to only allow incoming requests from Trading View
    
    * Add TradingView IP guard and trading pair interceptor

--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Get } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Body, Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
 import { BotRequest } from './entities/exchange';
 import { ExchangeService } from './exchange.service';
 import { assertIsString } from '../utils/helper';
+import { TradingViewGuard } from '../guard/tradingview.guard';
+import { ExchangeInterceptor } from '../guard/exchange.interceptor';
 
 @Controller('exchange')
 export class ExchangeController {
   constructor(private service: ExchangeService) {}
 
+  @UseInterceptors(ExchangeInterceptor)
   @Get('sell')
   async makeSellOrder(@Body() botReq: BotRequest): Promise<Observable<any>> {
     assertIsString(botReq.asset);
@@ -15,6 +18,7 @@ export class ExchangeController {
     return this.service.sell(botReq.asset, botReq.denominator, botReq.amount);
   }
 
+  @UseInterceptors(ExchangeInterceptor)
   @Get('buy')
   async makeBuyOrder(@Body() botReq: BotRequest): Promise<Observable<any>> {
     assertIsString(botReq.asset);
@@ -22,10 +26,16 @@ export class ExchangeController {
     return this.service.buy(botReq.asset, botReq.denominator, botReq.amount);
   }
 
+  @UseInterceptors(ExchangeInterceptor)
   @Get('clear')
   clearOrders(@Body() botReq: BotRequest): Observable<any> {
     assertIsString(botReq.asset);
     assertIsString(botReq.denominator);
     return this.service.clear(botReq.asset, botReq.denominator);
+  }
+
+  @Get('health')
+  health(): Observable<any> {
+    return of({ status: 'OK' });
   }
 }
