30c603b4d0293b37c1043620079b9fb0989fcca9
KalanyuZ <kalanyuz@users.noreply.github.com>
2021-05-08 22:42:14 +0900

    Feature/tradingview guard (#34)
    * Add guard to only allow incoming requests from Trading View
    
    * Add TradingView IP guard and trading pair interceptor

---
+++ src/guard/tradingview.guard.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ExecutionContext } from '@nestjs/common';
+import { TradingViewGuard } from './tradingview.guard';
+
+describe('TradingViewGuard', () => {
+  let service: TradingViewGuard;
+  let context: ExecutionContext;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TradingViewGuard],
+    }).compile();
+
+    service = module.get<TradingViewGuard>(TradingViewGuard);
+    // mocks the execution context with correct ip address
+    let request = {
+      headers: {
+        'x-forwarded-for': '52.32.178.7',
+      },
+    } as any;
+    let getRequestMock = jest.fn(() => request);
+    context = {
+      switchToHttp: () => ({
+        getRequest: getRequestMock,
+      }),
+    } as any;
+  });
+
+  it('should deny weird ipaddress(es)', () => {
+    expect(service.checkAllowedAddress('0')).toBeFalsy();
+    expect(service.checkAllowedAddress('')).toBeFalsy();
+  });
+
+  it('should accept allowed ipaddress(es)', () => {
+    expect(service.canActivate(context)).toBeTruthy();
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TradingViewGuard } from './guard/tradingview.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalGuards(new TradingViewGuard());
+  await app.listen(process.env.PORT);
 }
 bootstrap();
