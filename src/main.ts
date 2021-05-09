2179a5eb4432337d83643de3a7393384300ba929
KalanyuZ <kalanyuz@gmail.com>
2021-05-09 09:56:39 +0900

    Support enabling/disabling guard from env variable

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TradingViewGuard } from './guard/tradingview.guard';
+import 'source-map-support/register';
+
+const GUARDS = ['tradingview', 'none'].map((guard) => guard.toLowerCase());
+const GUARD_ERROR = `Authorization guard not specified. Set GUARD env variable to one of: ${GUARDS.toString()}`;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalGuards(new TradingViewGuard());
+  switch (process.env.GUARD.toLowerCase()) {
+    case 'tradingview':
+      app.useGlobalGuards(new TradingViewGuard());
+      break;
+    case 'none':
+      break;
+    default:
+      throw new Error(GUARD_ERROR);
+  }
   await app.listen(process.env.PORT);
 }
 bootstrap();
