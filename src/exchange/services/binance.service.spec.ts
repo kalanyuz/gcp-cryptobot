76774b1f7cf97e609df589cf8beea7d76a588aa6
KalanyuZ <kalanyuz@gmail.com>
2021-05-13 22:22:13 +0900

    Add price / asset tracking functions

---
+++ src/exchange/services/binance.service.spec.ts
@@ -0,0 +1,351 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
+import {
+  HttpModule,
+  HttpService,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '../../services/configs/configurations';
+import { BotConfigService } from '../../services/configs/botconfigs.service';
+import { SecretsService } from '../../services/secrets/secrets.service';
+import { BinanceExchange } from './binance.service';
+
+describe('ExchangeService', () => {
+  let service: BinanceExchange;
+  let httpClient: HttpService;
+  let secrets: any;
+  let config: BotConfigService;
+
+  /**
+   * response data definition
+   */
+  const orderResponse = {
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    config: {},
+    data: {
+      child_order_acceptance_id: 'JRF20150707-050237-639234',
+    },
+  };
+
+  const balanceResponse = {
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    config: {},
+    data: {
+      makerCommission: 0,
+      takerCommission: 0,
+      buyerCommission: 0,
+      sellerCommission: 0,
+      canTrade: true,
+      canWithdraw: false,
+      canDeposit: false,
+      updateTime: 1620902925998,
+      accountType: 'SPOT',
+      balances: [
+        {
+          asset: 'BNB',
+          free: '1000.00000000',
+          locked: '50.00000000',
+        },
+        {
+          asset: 'BTC',
+          free: '0.99898700',
+          locked: '0.00000000',
+        },
+        {
+          asset: 'BUSD',
+          free: '10000.00000000',
+          locked: '0.00000000',
+        },
+        {
+          asset: 'ETH',
+          free: '100.00000000',
+          locked: '0.00000000',
+        },
+        {
+          asset: 'TRX',
+          free: '0.00000000',
+          locked: '0.00000000',
+        },
+        {
+          asset: 'USDT',
+          free: '10050.01638856',
+          locked: '0.00000000',
+        },
+      ],
+      permissions: ['SPOT'],
+    },
+  };
+
+  const allAsset = {
+    balances: [
+      {
+        currency_code: 'BNB',
+        amount: 1050.0,
+        available: 1000.0,
+      },
+      {
+        currency_code: 'BTC',
+        amount: 0.998987,
+        available: 0.998987,
+      },
+      {
+        currency_code: 'BUSD',
+        amount: 10000.0,
+        available: 10000.0,
+      },
+      {
+        currency_code: 'ETH',
+        amount: 100.0,
+        available: 100.0,
+      },
+      {
+        currency_code: 'USDT',
+        amount: 10050.01638856,
+        available: 10050.01638856,
+      },
+    ],
+    total: {
+      amount: 0.07073226,
+      currency_code: 'BTC',
+    },
+  };
+
+  const tickerBTC = {
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    config: {},
+    data: {
+      symbol: 'BTCUSDT',
+      price: '49663.56000000',
+    },
+  };
+
+  beforeEach(async () => {
+    secrets = {
+      getSecret: jest.fn().mockResolvedValue('huh?'),
+    };
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule,
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [configuration],
+        }),
+      ],
+      providers: [
+        BinanceExchange,
+        BotConfigService,
+        { provide: SecretsService, useValue: secrets },
+      ],
+    }).compile();
+
+    config = module.get<BotConfigService>(BotConfigService);
+    httpClient = module.get<HttpService>(HttpService);
+
+    service = new BinanceExchange(httpClient, config, secrets);
+  });
+
+  /**
+   * The key to making this test work is passing it the the done keyword, otherwise it will finish before the data is emitted.
+   * https://fireship.io/snippets/testing-rxjs-observables-with-jest/
+   */
+  // it('should extract price of a crypto', (done) => {
+  //   jest.spyOn(httpClient, 'get').mockReturnValue(of(tickerBTC));
+  //   const price = service.getPrice('BTC', 'USDT');
+
+  //   price.subscribe({
+  //     next: (x) => {
+  //       expect(x).toMatchObject({
+  //         amount: 49663.56,
+  //         currency_code: 'USDT',
+  //       });
+  //     },
+  //     complete: () => done(),
+  //   });
+  // });
+
+  it('should reject when price data is incorrect', (done) => {
+    jest.spyOn(httpClient, 'get').mockReturnValue(
+      of({
+        status: 200,
+        statusText: 'OK',
+        headers: {},
+        config: {},
+        data: {
+          symbol: 'BTCUSDA',
+          price: '49663.56000000',
+        },
+      }),
+    );
+    const price = service.getPrice('BTC', 'USDT');
+
+    price.subscribe({
+      complete: () => done(),
+      error: (error) => {
+        expect(error).toMatchObject(
+          new HttpException('Failed to get price info', HttpStatus.BAD_REQUEST),
+        );
+        done();
+      },
+    });
+  });
+
+  it('should filter out balance that is 0', (done) => {
+    jest.spyOn(service, 'getPrice').mockReturnValue(
+      of({
+        amount: 0.02357742,
+        currency_code: 'BTC',
+      }),
+    );
+    jest.spyOn(httpClient, 'get').mockReturnValueOnce(of(balanceResponse));
+    const result = service.getBalance('BTC');
+
+    result.subscribe({
+      next: (x) => {
+        expect(x.balances).toMatchObject(allAsset.balances);
+      },
+      complete: () => done(),
+    });
+  });
+
+  it('should correctly calculate output balance', (done) => {
+    jest.spyOn(service, 'getPrice').mockReturnValue(
+      of({
+        amount: 0.02357742,
+        currency_code: 'BTC',
+      }),
+    );
+    jest.spyOn(httpClient, 'get').mockReturnValueOnce(of(balanceResponse));
+    const result = service.getBalance('BTC');
+
+    result.subscribe({
+      next: (x) => {
+        expect(x.total.amount).toBeCloseTo(1.09329668);
+        expect(x.total.currency_code).toBe('BTC');
+      },
+      complete: () => done(),
+    });
+  });
+
+  // it('should find the amount of asset to sell when not specified', async (done) => {
+  //   const getBalance = jest
+  //     .spyOn(service, 'getBalance')
+  //     .mockReturnValue(of(allAsset));
+
+  //   jest.spyOn(httpClient, 'post').mockReturnValueOnce(of(orderResponse));
+  //   const result = await service.sell('BTC', 'JPY');
+
+  //   expect(result).toEqual(orderResponse);
+  //   expect(getBalance).toBeCalledWith('JPY');
+  //   expect(getBalance).toBeCalledTimes(1);
+  //   done();
+  // });
+
+  // it('should not find the amount of asset to sell when specified', async (done) => {
+  //   const getBalance = jest
+  //     .spyOn(service, 'getBalance')
+  //     .mockReturnValue(of('' as any));
+
+  //   jest.spyOn(httpClient, 'post').mockReturnValueOnce(of(orderResponse));
+  //   const result = await service.sell('BTC', 'JPY', 0.01);
+
+  //   expect(result).toEqual(orderResponse);
+  //   expect(getBalance).not.toBeCalled();
+  //   done();
+  // });
+
+  // it('should throw when asset to sell is not found', async (done) => {
+  //   const getBalance = jest
+  //     .spyOn(service, 'getBalance')
+  //     .mockReturnValue(of(allAsset));
+
+  //   jest.spyOn(httpClient, 'post').mockReturnValueOnce(of(orderResponse));
+  //   const result = service.sell('DOGE', 'JPY');
+
+  //   expect(result).rejects.toThrow(
+  //     new HttpException(
+  //       'Could not find an asset to sell.',
+  //       HttpStatus.BAD_REQUEST,
+  //     ),
+  //   );
+  //   expect(getBalance).toBeCalledWith('JPY');
+  //   expect(getBalance).toBeCalledTimes(1);
+  //   done();
+  // });
+
+  // it('should find the amount of asset to buy when not specified', async (done) => {
+  //   const getBalance = jest
+  //     .spyOn(service, 'getBalance')
+  //     .mockReturnValue(of(allAsset));
+
+  //   jest.spyOn(httpClient, 'post').mockReturnValueOnce(of(orderResponse));
+  //   const result = await service.buy('ETH', 'BTC');
+
+  //   expect(result).toEqual(orderResponse);
+  //   expect(getBalance).toBeCalledWith('BTC');
+  //   expect(getBalance).toBeCalledTimes(1);
+  //   done();
+  // });
+
+  // it('should not find the amount of asset to buy when specified', async (done) => {
+  //   const getBalance = jest
+  //     .spyOn(service, 'getBalance')
+  //     .mockReturnValue(of('' as any));
+
+  //   jest.spyOn(httpClient, 'post').mockReturnValueOnce(of(orderResponse));
+  //   const result = await service.buy('DOGE', 'BTC', 0.01);
+
+  //   expect(result).toEqual(orderResponse);
+  //   expect(getBalance).not.toBeCalled();
+  //   done();
+  // });
+
+  // it('should throw when asset to buy with is not found', async (done) => {
+  //   const getBalance = jest
+  //     .spyOn(service, 'getBalance')
+  //     .mockReturnValue(of(allAsset));
+
+  //   jest.spyOn(httpClient, 'post').mockReturnValueOnce(of(orderResponse));
+  //   const result = service.buy('BTC', 'DOGE');
+
+  //   expect(result).rejects.toThrow(
+  //     new HttpException(
+  //       'Could not calculate total available asset',
+  //       HttpStatus.INTERNAL_SERVER_ERROR,
+  //     ),
+  //   );
+  //   expect(getBalance).toBeCalledWith('DOGE');
+  //   expect(getBalance).toBeCalledTimes(1);
+  //   done();
+  // });
+
+  // it('should rebalance correctly when amount is not specified', async (done) => {
+  //   jest.spyOn(service, 'getBalance').mockReturnValue(of(allAsset));
+  //   const buyRequest = jest
+  //     .spyOn(httpClient, 'post')
+  //     .mockReturnValueOnce(of(orderResponse));
+  //   const result = await service.buy('ETH', 'BTC');
+
+  //   expect(result).toEqual(orderResponse);
+
+  //   expect(buyRequest).toHaveBeenCalledWith(
+  //     expect.anything(),
+  //     JSON.stringify({
+  //       product_code: `ETH_BTC`,
+  //       child_order_type: 'MARKET',
+  //       side: 'BUY',
+  //       size: 0.017683065,
+  //       time_in_force: 'GTC',
+  //     }),
+  //     expect.anything(),
+  //   );
+  //   done();
+  // });
+});
---
+++ src/exchange/services/binance.service.ts
@@ -0,0 +1,284 @@
+import {
+  HttpException,
+  HttpService,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import {
+  map,
+  filter,
+  mergeMap,
+  toArray,
+  concatMap,
+  reduce,
+  catchError,
+} from 'rxjs/operators';
+import { BotConfigService } from '../../services/configs/botconfigs.service';
+import { config, Observable, of, throwError } from 'rxjs';
+import { ExchangeService } from '../exchange.service';
+import * as crypto from 'crypto';
+import { forkJoin } from 'rxjs';
+import { SecretsService } from '../../services/secrets/secrets.service';
+import {
+  BitFlyerAsset,
+  BitFlyerBalance,
+  BitFlyerSignature,
+} from './bitflyer.entities';
+
+@Injectable()
+export class BinanceExchange extends ExchangeService {
+  baseURL: string = 'https://testnet.binance.vision';
+  key = { 'X-MBX-APIKEY': '' };
+  secret: string = '';
+
+  constructor(
+    httpService: HttpService,
+    private configs: BotConfigService,
+    secretService: SecretsService,
+  ) {
+    super(httpService, secretService);
+    if (configs.tradeCurrency !== 'JPY') {
+      throw new HttpException(
+        'BitFlyer module currently supports only JPY based trading pairs',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    const apiKeyName: string = configs.settings['api_keyname'];
+    const secretKeyName: string = configs.settings['secret_keyname'];
+
+    secretService
+      .getSecret(apiKeyName)
+      .then((key) => {
+        this.key['X-MBX-APIKEY'] = key;
+      })
+      .catch((error) => console.error(error));
+
+    secretService
+      .getSecret(secretKeyName)
+      .then((key) => {
+        this.secret = key;
+      })
+      .catch((error) => console.error(error));
+  }
+
+  private createSignature(query: string): string {
+    const signature = crypto
+      .createHmac('sha256', this.secret)
+      .update(query)
+      .digest('hex');
+
+    return signature;
+  }
+
+  getPrice(ofProduct: string, priceIn: string): Observable<any> {
+    const path = '/api/v3/ticker/price?';
+    const response = this.httpService.get(
+      `${this.baseURL}${path}?symbol=${ofProduct}${priceIn}`,
+    );
+    const price = response.pipe(
+      map((x) => x.data),
+      map((x) => {
+        if (x['symbol'] !== `${ofProduct}${priceIn}`) {
+          console.error(
+            'Price info returned different from what is requested.',
+          );
+          throw new HttpException(
+            'Failed to get price info',
+            HttpStatus.EXPECTATION_FAILED,
+          );
+        }
+        try {
+          const price = parseFloat(x['price']);
+          if (isNaN(price)) throw 'NaN';
+
+          return {
+            amount: price,
+            currency_code: priceIn,
+          };
+        } catch {
+          throw new HttpException(
+            'Failed to get price info',
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+      }),
+      catchError((err) => {
+        return throwError(err);
+      }),
+    );
+    return price;
+  }
+
+  getBalance(priceIn: string): Observable<any> {
+    const path = '/api/v3/account?';
+    const query = `timestamp=${new Date().getTime().toString()}`;
+    const signature = this.createSignature(query);
+    const response = this.httpService.get(
+      `${this.baseURL}${path}${query}&signature=${signature}`,
+      {
+        headers: this.key,
+      },
+    );
+
+    const balances = response.pipe(
+      mergeMap((x) => x.data.balances),
+      map((x) => ({
+        currency_code: x['asset'],
+        amount: parseFloat(x['free']) + parseFloat(x['locked']),
+        available: parseFloat(x['free']),
+      })),
+      filter((x) => x['amount'] > 0),
+      toArray<any>(),
+      catchError((err) => {
+        console.error(err.response.data);
+        return throwError(err);
+      }),
+    );
+
+    const total = balances.pipe(
+      mergeMap((item) => item),
+      concatMap((item) => {
+        if (item['currency_code'] !== priceIn) {
+          return this.getPrice(item['currency_code'], priceIn);
+        } else {
+          return of(item);
+        }
+      }),
+      reduce(
+        (current, x) => {
+          current.amount += x.amount;
+          return current;
+        },
+        {
+          currency_code: priceIn,
+          amount: 0,
+        },
+      ),
+    );
+
+    return forkJoin({
+      balances,
+      total,
+    });
+  }
+
+  async buy(
+    asset: string,
+    using: string,
+    amount?: number,
+    stopLoss?: number,
+  ): Promise<any> {
+    /* get balance, compute total asset, allocate */
+    /* if amount is not specified, getBalance and check rebalancing configuration */
+    if (amount === undefined) {
+      try {
+        const myAsset = await this.getBalance(using)
+          .pipe(
+            map((x) => x.total),
+            filter((x) => x.currency_code === using),
+          )
+          .toPromise();
+
+        amount = myAsset['amount'];
+        const ratio =
+          this.configs.rebalanceProfiles
+            ?.filter((x) => x.asset === asset)
+            .map((x) => x.ratio as number)[0] ?? 1;
+
+        amount *= ratio;
+      } catch (error) {
+        throw new HttpException(
+          'Could not calculate total available asset',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+
+    const path = '/v1/me/sendchildorder';
+    const requestBody = JSON.stringify({
+      product_code: `${asset}_${using}`,
+      child_order_type: 'MARKET',
+      side: 'BUY',
+      size: amount,
+      time_in_force: 'GTC',
+    });
+    const signature = this.createSignature('');
+    const response = this.httpService
+      .post(this.baseURL + path, requestBody, {
+        headers: this.key,
+      })
+      .pipe(
+        catchError((err) => {
+          console.error(err.response.data);
+          return throwError(err);
+        }),
+      )
+      .toPromise();
+
+    return response;
+  }
+
+  async sell(asset: string, sellFor: string, amount?: number): Promise<any> {
+    if (amount === undefined) {
+      try {
+        const myAsset = await this.getBalance(sellFor)
+          .pipe(
+            mergeMap((x) => x.balances),
+            filter((x) => x['currency_code'] === asset),
+          )
+          .toPromise();
+
+        amount = myAsset['amount'];
+      } catch (error) {
+        throw new HttpException(
+          'Could not find an asset to sell.',
+          HttpStatus.EXPECTATION_FAILED,
+        );
+      }
+    }
+
+    const path = '/v1/me/sendchildorder';
+    const requestBody = JSON.stringify({
+      product_code: `${asset}_${sellFor}`,
+      child_order_type: 'MARKET',
+      side: 'SELL',
+      size: amount,
+      time_in_force: 'GTC',
+    });
+    const signature = this.createSignature('');
+    const response = this.httpService
+      .post(this.baseURL + path, requestBody, {
+        headers: this.key,
+      })
+      .pipe(
+        catchError((err) => {
+          console.error(err.response.data);
+          return throwError(err);
+        }),
+      )
+      .toPromise();
+
+    return response;
+  }
+
+  clear(asset: string, denominator: string): Promise<any> {
+    const path = '/v1/me/cancelallchildorders';
+    const requestBody = JSON.stringify({
+      product_code: `${asset}_${denominator}`,
+    });
+    const signature = this.createSignature('');
+    const response = this.httpService
+      .post(this.baseURL + path, requestBody, {
+        headers: this.key,
+      })
+      .pipe(
+        catchError((err) => {
+          console.error(err.response.data);
+          return throwError(err);
+        }),
+      )
+      .toPromise();
+
+    return response;
+  }
+}
